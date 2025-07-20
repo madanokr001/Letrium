7b7a52c0fcaac3c6303e1b3f1bb61b5c9c52a52a
cybermad <madanokr002@gmail.com>
2025-07-20 21:23:43 +0900

    Delete lib directory

--- lib/System/textspech.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-def audio(text: str) -> str:
-    try:
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-    except Exception as e:
-        return f"{e}"
